084ded7f50f71da206db70e38466c0bfa4958d3a
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-03-06 10:52:04 +0530

    Styling

--- app/Components/Payment.tsx
+++ app/Components/Payment.tsx
@@ -1,3 +1,4 @@
+// Import necessary components and assets
 import React, { useState } from "react";
 import { useRoute } from "@react-navigation/native";
 import {
@@ -6,35 +7,37 @@ import {
   TextInput,
   TouchableOpacity,
   StyleSheet,
+  Image, // Import Image component
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "expo-router";
 
+// Import image assets for net banking
+// import phonePeImage from "../../assets/images/phonePe.png";
+// import gpayImage from "../../assets/images/gpay.png";
+// import paytmImage from "../../assets/images/paytm.png";
+// import upiImage from "../../assets/images/Upi.png";
+
 const Payment = () => {
-  // Accessing the route and navigation objects
   const route = useRoute();
   const navigation = useNavigation();
-
-  // Extracting the product object from route params
   const { product } = route.params;
 
-  // State variables for managing payment details
   const [mobileNumber, setMobileNumber] = useState("");
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("");
   const [cardNumber, setCardNumber] = useState("");
   const [validThru, setValidThru] = useState("");
   const [cvv, setCvv] = useState("");
 
-  // Handler for mobile number change
   const handleMobileNumberChange = (text) => {
     setMobileNumber(text);
   };
 
-  // Handler for initiating payment
   const handlePayment = () => {};
 
   return (
     <View style={styles.container}>
+      {/* Header */}
       <View style={styles.headerContainer}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <AntDesign name="arrowleft" size={24} color="black" />
@@ -44,10 +47,19 @@ const Payment = () => {
           <AntDesign name="questioncircleo" size={24} color="black" />
         </TouchableOpacity>
       </View>
+
+      {/* Total Amount */}
       <Text style={styles.totalAmount}>Total Amount: ₹ {product.rupees}</Text>
+
+      {/* Discount */}
       <Text style={styles.discount}>20% Off</Text>
+
+      {/* Payment Method Container */}
       <View style={styles.paymentMethodContainer}>
+        {/* Payment Method Header */}
         <Text style={styles.paymentMethodHeader}>Payment Method</Text>
+
+        {/* Credit/Debit Card Option */}
         <TouchableOpacity
           style={[
             styles.paymentMethodOption,
@@ -58,8 +70,10 @@ const Payment = () => {
           onPress={() => setSelectedPaymentMethod("creditDebitCard")}
         >
           <Text style={styles.paymentMethodText}>Credit/Debit Card</Text>
+          {/* Card Details */}
           {selectedPaymentMethod === "creditDebitCard" && (
             <View style={styles.cardDetailsContainer}>
+              {/* Input fields for card details */}
               <TextInput
                 style={styles.input}
                 placeholder="Card Number"
@@ -83,40 +97,73 @@ const Payment = () => {
             </View>
           )}
         </TouchableOpacity>
+
+        {/* Net Banking Option */}
         <TouchableOpacity
           style={styles.paymentMethodOption}
           onPress={() => setSelectedPaymentMethod("netBanking")}
         >
           <Text style={styles.paymentMethodText}>Net Banking</Text>
         </TouchableOpacity>
+
+        {/* Netbanking Options */}
         {selectedPaymentMethod === "netBanking" && (
           <View style={styles.walletsContainer}>
-            <Text style={styles.walletsHeader}>Wallets</Text>
+            <Text style={styles.walletsHeader}>Net Banking</Text>
             <View style={styles.walletsOptions}>
+              {/* PhonePe */}
               <TouchableOpacity onPress={() => {}} style={styles.walletOption}>
-                <Text>PhonePe</Text>
+                <Image
+                  source={"../../assets/images/phonePe.png"}
+                  style={styles.walletImage}
+                />
               </TouchableOpacity>
+
+              {/* GPay */}
               <TouchableOpacity onPress={() => {}} style={styles.walletOption}>
-                <Text>Paytm Payments Bank</Text>
+                <Image
+                  source={"../../assets/images/gpay.png"}
+                  style={styles.walletImage}
+                />
+              </TouchableOpacity>
+
+              {/* Paytm */}
+              <TouchableOpacity onPress={() => {}} style={styles.walletOption}>
+                <Image
+                  source={"../../assets/images/paytm.png"}
+                  style={styles.walletImage}
+                />
+              </TouchableOpacity>
+
+              {/* UPI */}
+              <TouchableOpacity onPress={() => {}} style={styles.walletOption}>
+                <Image
+                  source={"../../assets/images/Upi.jpg"}
+                  style={styles.walletImage}
+                />
               </TouchableOpacity>
             </View>
-            <Text style={styles.mobileNumberLabel}>
-              Paytm Linked Mobile Number
-            </Text>
-            <View style={styles.mobileNumberContainer}>
-              <TextInput
-                style={styles.input}
-                placeholder="Enter Mobile Number"
-                value={mobileNumber}
-                onChangeText={handleMobileNumberChange}
-                keyboardType="numeric"
-              />
-              <TouchableOpacity onPress={() => {}} style={styles.linkButton}>
-                <Text style={styles.linkButtonText}>Link</Text>
-              </TouchableOpacity>
-            </View>
+
+            {/* Mobile Number for Net Banking */}
+            {/* <Text style={styles.mobileNumberLabel}>
+            Paytm Linked Mobile Number
+          </Text> */}
+            {/* <View style={styles.mobileNumberContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter Mobile Number"
+              value={mobileNumber}
+              onChangeText={handleMobileNumberChange}
+              keyboardType="numeric"
+            />
+            <TouchableOpacity onPress={() => {}} style={styles.linkButton}>
+              <Text style={styles.linkButtonText}>Link</Text>
+            </TouchableOpacity>
+          </View> */}
           </View>
         )}
+
+        {/* Wallets Option */}
         <TouchableOpacity
           style={styles.paymentMethodOption}
           onPress={() => setSelectedPaymentMethod("wallet")}
@@ -124,17 +171,29 @@ const Payment = () => {
           <Text style={styles.paymentMethodText}>Wallets</Text>
         </TouchableOpacity>
       </View>
+
+      {/* Wallet Options */}
       {selectedPaymentMethod === "wallet" && (
         <View style={styles.walletsContainer}>
-          <Text style={styles.walletsHeader}>Wallets</Text>
+          <Text style={styles.walletsHeader}>Net Banking</Text>
           <View style={styles.walletsOptions}>
+            {/* PhonePe */}
             <TouchableOpacity onPress={() => {}} style={styles.walletOption}>
-              <Text>PhonePe</Text>
+              <Image
+                source={"../../assets/images/phonePe.png"}
+                style={styles.walletImage}
+              />
             </TouchableOpacity>
+            {/* UPI */}
             <TouchableOpacity onPress={() => {}} style={styles.walletOption}>
-              <Text>Paytm Payments Bank</Text>
+              <Image
+                source={"../../assets/images/Upi.jpg"}
+                style={styles.walletImage}
+              />
             </TouchableOpacity>
           </View>
+
+          {/* Mobile Number for Net Banking */}
           <Text style={styles.mobileNumberLabel}>
             Paytm Linked Mobile Number
           </Text>
@@ -152,6 +211,7 @@ const Payment = () => {
           </View>
         </View>
       )}
+      {/* Pay Button */}
       <TouchableOpacity style={styles.payButton} onPress={handlePayment}>
         <Text style={styles.payButtonText}>Pay ₹ {product.rupees}</Text>
       </TouchableOpacity>
@@ -163,7 +223,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: "#f0f0f0",
+    backgroundColor: "#ffffff",
   },
   headerContainer: {
     flexDirection: "row",
@@ -183,7 +243,7 @@ const styles = StyleSheet.create({
   },
   discount: {
     fontSize: 18,
-    color: "green",
+    color: "#00cc00",
     marginBottom: 20,
   },
   paymentMethodContainer: {
@@ -196,12 +256,12 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   paymentMethodOption: {
-    backgroundColor: "#fff",
+    backgroundColor: "#f0f0f0",
     padding: 15,
     borderRadius: 10,
     marginBottom: 10,
     borderWidth: 1,
-    borderColor: "#ccc",
+    borderColor: "#cccccc",
   },
   paymentMethodText: {
     fontSize: 16,
@@ -217,13 +277,17 @@ const styles = StyleSheet.create({
   },
   walletsOptions: {
     flexDirection: "row",
+    flexWrap: "wrap", // Added to wrap wallet options to the next line if they exceed the width
     justifyContent: "space-between",
     marginBottom: 10,
   },
   walletOption: {
-    backgroundColor: "#f0f0f0",
-    padding: 15,
-    borderRadius: 10,
+    marginBottom: 10,
+  },
+  walletImage: {
+    width: 100,
+    height: 50,
+    resizeMode: "contain",
   },
   mobileNumberLabel: {
     fontSize: 16,
@@ -236,7 +300,7 @@ const styles = StyleSheet.create({
   },
   input: {
     flex: 1,
-    backgroundColor: "#f0f0f0",
+    backgroundColor: "#f5f5f5",
     borderRadius: 10,
     padding: 15,
     marginBottom: 10,
